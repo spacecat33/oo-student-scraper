72546a6e6fb2839e7a39def18d620dd7f07d6fd1
Janan Akkad <jananakkad@hotmail.co.uk>
2022-06-16 15:56:39 +0100

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,32 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      student = self.new(student)
+      # student.name = post.css("h4").text
+      # student.location = post.css("p").text
+
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
 
+# ("./fixtures/student-site/index.html")
